fa145d5ddeb7a721d918498a48a91b1bdc93bf03
Manav Sharma <manavsharma@192.168.1.4>
2025-07-11 16:32:50 +0530

    complete project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
-import { ReactNode } from "react";
-import type { Metadata } from "next";
-import { ClerkProvider } from "@clerk/nextjs";
-import { Inter } from "next/font/google";
+import { ReactNode } from 'react';
+import type { Metadata } from 'next';
+import { ClerkProvider } from '@clerk/nextjs';
+import { Inter } from 'next/font/google';
 
-import "@stream-io/video-react-sdk/dist/css/styles.css";
-import "react-datepicker/dist/react-datepicker.css";
-import "./globals.css";
-import { Toaster } from "@/components/ui/toaster";
+import '@stream-io/video-react-sdk/dist/css/styles.css';
+import 'react-datepicker/dist/react-datepicker.css';
+import './globals.css';
+import { Toaster } from '@/components/ui/toaster';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "YOOM",
-  description: "Video calling App",
+  title: 'YOOM',
+  description: 'Video calling App',
   icons: {
-    icon: "/icons/logo.svg",
+    icon: '/icons/logo.svg',
   },
 };
 
@@ -23,26 +23,26 @@ export default function RootLayout({
 }: Readonly<{ children: ReactNode }>) {
   return (
     <html lang="en">
-      <ClerkProvider
-        appearance={{
-          layout: {
-            socialButtonsVariant: "iconButton",
-            logoImageUrl: "/icons/yoom-logo.svg",
-          },
-          variables: {
-            colorText: "#fff",
-            colorPrimary: "#0E78F9",
-            colorBackground: "#1C1F2E",
-            colorInputBackground: "#252A41",
-            colorInputText: "#fff",
-          },
-        }}
-      >
-        <body className={`${inter.className} bg-dark-2`}>
+      <body className={`${inter.className} bg-dark-2`}>
+        <ClerkProvider
+          appearance={{
+            layout: {
+              socialButtonsVariant: 'iconButton',
+              logoImageUrl: '/icons/yoom-logo.svg',
+            },
+            variables: {
+              colorText: '#fff',
+              colorPrimary: '#0E78F9',
+              colorBackground: '#1C1F2E',
+              colorInputBackground: '#252A41',
+              colorInputText: '#fff',
+            },
+          }}
+        >
           <Toaster />
           {children}
-        </body>
-      </ClerkProvider>
+        </ClerkProvider>
+      </body>
     </html>
   );
 }
